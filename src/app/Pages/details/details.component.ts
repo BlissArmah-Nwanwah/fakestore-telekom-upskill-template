aff3976a042a672a0b4a4a5fe71e639f9f6810d6
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2024-04-17 23:59:08 +0000

    homepage designed with product fetched

--- src/app/Pages/details/details.component.ts
+++ src/app/Pages/details/details.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ProductData } from '../../Services/product-data';
+import { ProductService } from '../../Services/product.service';
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+    selector: 'app-details',
+    standalone: true,
+    templateUrl: './details.component.html',
+    styleUrl: './details.component.css',
+    imports: [CommonModule, FooterComponent, RouterLink, NavbarComponent]
 })
 export class DetailsComponent {
+  selectedProduct: ProductData | null = null;
+  loading: boolean = false;
+  selectedSize: string = '';
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+
+  ngOnInit(): void {
+
+    this.getSelectedProduct();
+  }
+
+  getSelectedProduct() {
+    this.selectedProduct = this.productService.getSelectedProduct();
+
+  }
+
+  changeBgColor(size: string) {
+    this.selectedSize = size;
+    alert('Selected Size: ' + this.selectedSize);
+  }
+
+  isSizeSelected(size: string): boolean {
+    return this.selectedSize === size;
+  }
 
 }
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ProductData, cartProductData } from '../../Services/product-data';
+import { Router } from '@angular/router';
+import { ProductService } from '../../Services/product.service';
+import { EMPTY, catchError, finalize } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -9,5 +14,86 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  images = [
+    '../../../../assets/images/shop-hero-1-product-slide-1.png',
+    '../../../../assets/images/s1.jpg',
+    '../../../../assets/images/s2.png',
+    // Add more image paths as needed
+  ];
+  currentIndex = 0;
+
+  
+  products: ProductData[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 8;
+  loading: boolean = false;
+
+
+  constructor(private router:Router,private productService: ProductService){}
+
+  ngOnInit(): void {
+  this.getMenCategory()
+  }
+
+  getMenCategory(): void {
+  this.loading = true;
+  this.productService.getProducts()
+  .pipe(
+  catchError(() => EMPTY),
+        // finalize(() => this.loading = false)
+  )
+  .subscribe(products => {
+  this.products = products;
+  this.loading = false;
+  });
+  }
+
+  onProductSelectedToCart(product: cartProductData): void {
+    // add product to cart
+  this.productService.setSelectedProduct(product);
+  }
+
+  onProductSelectDetail(): void {
+    // Navigate to the details page with the product ID as a parameter
+  this.router.navigate(["/details"]);
+  }
+
+  
+
+  nextPage(): void {
+  if (this.hasNextPage()) {
+  this.loading = true;
+  this.currentPage++;
+  this.getMenCategory();
+  }
+  }
+
+  prevPage(): void {
+  if (this.hasPreviousPage()) {
+  this.loading = true;
+  this.currentPage--;
+  this.getMenCategory();
+  }
+  }
+
+  hasNextPage(): boolean {
+  return (this.currentPage * this.itemsPerPage) < this.products.length;
+  }
+
+  hasPreviousPage(): boolean {
+  return this.currentPage > 1;
+  }
+
+  get paginationVisible(): boolean {
+  return this.products.length > this.itemsPerPage;
+  }
+
+  get displayedProducts(): ProductData[] {
+  const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+  const endIndex = startIndex + this.itemsPerPage;
+  return this.products.slice(startIndex, endIndex);
+  }
+
 
 }
