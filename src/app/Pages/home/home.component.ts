f5f02389dcb7a57ba1d264b76c22ddfe21821ca4
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2024-04-19 14:19:59 +0000

    angular snackbar used

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -6,66 +6,89 @@ import { Router } from '@angular/router';
 import { EMPTY } from 'rxjs';
 import { catchError, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {
+  MatSnackBarModule,
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
-  standalone:true,
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [NavbarComponent,ReactiveFormsModule,MatButtonModule,MatPaginatorModule]
+  imports: [
+    NavbarComponent,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+  ],
 })
 export class HomeComponent implements OnInit {
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
   searchForm!: FormGroup;
   products: ProductData[] = [];
   currentPage: number = 1;
   itemsPerPage: number = 8;
   loading: boolean = false;
-  productLength:number = 0;
+  productLength: number = 0;
+  durationInSeconds = 2;
 
   constructor(
     private router: Router,
     private productService: ProductService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.allProduct();
     this.searchForm = this.formBuilder.group({
-      searchTerm: ['']
+      searchTerm: [''],
     });
 
-    this.searchForm.get('searchTerm')?.valueChanges
-    .pipe(
-      debounceTime(300), // Debounce for 300 milliseconds
-      distinctUntilChanged() // Only emit if the value has changed
-    )
-    .subscribe(() => {
-      this.searchProducts();
-    });
+    this.searchForm
+      .get('searchTerm')
+      ?.valueChanges.pipe(
+        debounceTime(300), // Debounce for 300 milliseconds
+        distinctUntilChanged() // Only emit if the value has changed
+      )
+      .subscribe(() => {
+        this.searchProducts();
+      });
   }
 
   allProduct(): void {
     this.loading = true;
-    this.productService.getProducts()
-      .pipe(
-        catchError(() => EMPTY)
-      )
-      .subscribe(products => {
+    this.productService
+      .getProducts()
+      .pipe(catchError(() => EMPTY))
+      .subscribe((products) => {
         this.products = products;
-        this.productLength = Math.ceil(products.length/8);
+        this.productLength = Math.ceil(products.length / 8);
         this.loading = false;
       });
   }
 
   onProductSelectedToCart(product: cartProductData): void {
     this.productService.setSelectedProductToCart(product);
+    this.openSnackBar('Item added to cart', 'Close')
   }
 
   onProductSelectDetail(product: cartProductData): void {
     this.productService.setSelectedProduct(product);
-    this.router.navigate(["/details", product.id]);
+    this.router.navigate(['/details', product.id]);
   }
 
   handlePageEvent(event: PageEvent) {
@@ -84,16 +107,24 @@ export class HomeComponent implements OnInit {
 
   searchProducts(): void {
     const searchTerm = this.searchForm.value.searchTerm.trim().toLowerCase();
-    
+
     if (searchTerm === '') {
       // Reset products to all products when search term is empty
       this.allProduct();
     } else {
-      this.products = this.products.filter(product =>
+      this.products = this.products.filter((product) =>
         product.title.toLowerCase().includes(searchTerm)
       );
     }
-  
+
     this.currentPage = 1;
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action,{
+      duration: this.durationInSeconds * 1000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 }
